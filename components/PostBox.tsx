50b09665540a4cdac112fd016eae99b395c02101
Thomas Selden <thomasselden42@gmail.com>
2022-06-01 17:59:17 -0400

    created form for post

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,5 +1,5 @@
 import { useSession } from "next-auth/react";
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "./Avatar";
 import { LinkIcon, PhotographIcon } from "@heroicons/react/outline";
 import { useForm } from "react-hook-form";
@@ -13,6 +13,7 @@ type FormData = {
 
 function PostBox() {
   const { data: session } = useSession();
+  const [imageBoxOpen, setImageBoxOpen] = useState(false);
   const {
     register,
     setValue,
@@ -21,8 +22,15 @@ function PostBox() {
     formState: { errors },
   } = useForm<FormData>();
 
+  const onSubmit = handleSubmit(async (FormData) => {
+    console.log(FormData);
+  });
+
   return (
-    <form className="sticky top-16 z-50 border border-gray-300 bg-white p-2">
+    <form
+      onSubmit={onSubmit}
+      className="sticky top-16 z-50 border border-gray-300 bg-white p-2"
+    >
       <div className="flex items-center space-x-3">
         <Avatar />
         <input
@@ -35,7 +43,12 @@ function PostBox() {
           }
         />
 
-        <PhotographIcon className="h-6 cursor-pointer text-gray-300" />
+        <PhotographIcon
+          onClick={() => setImageBoxOpen(!imageBoxOpen)}
+          className={`h-6 cursor-pointer text-gray-300 ${
+            imageBoxOpen && "text-blue-300"
+          }`}
+        />
         <LinkIcon className="h-6 text-gray-300" />
       </div>
 
@@ -43,23 +56,56 @@ function PostBox() {
         <div className="flex flex-col py-2">
           {/* body */}
           <div className="flex items-center px-2">
-            <p className="min-w-[90px]">Body</p>
+            <p className="min-w-[90px]">Body:</p>
             <input
               className="m-2 flex-1 bg-blue-50 p-2 outline-none"
               {...register("postBody")}
               type="text"
-              placeholder="Text (Optional)"
+              placeholder="Text"
             />
           </div>
+
+          {/* subreddit */}
           <div className="flex items-center px-2">
-            <p className="min-w-[90px]">Subreddit</p>
+            <p className="min-w-[90px]">Subreddit:</p>
             <input
               className="m-2 flex-1 bg-blue-50 p-2 outline-none"
-              {...register("subreddit")}
+              {...register("subreddit", {required: true })}
               type="text"
               placeholder="i.e. NextJS"
             />
           </div>
+
+          {imageBoxOpen && (
+            <div className="flex items-center px-2">
+              <p className="min-w-[90px]">Image URL:</p>
+              <input
+                className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+                {...register("postImage")}
+                type="text"
+                placeholder="Optional..."
+              />
+            </div>
+          )}
+
+          {Object.keys(errors).length > 0 && (
+            <div className="space-y-2 p-2 text-red-500">
+              {errors.postTitle?.type === "required" && (
+                <p>- A Post Title is required</p>
+              )}
+              {errors.subreddit?.type === "required" && (
+                <p>- A Subreddit is required</p>
+              )}
+            </div>
+          )}
+          {!!watch("postTitle") && (
+            <button
+              type="submit"
+              className="w-full rounded-full bg-blue-400 p-2 text-white"
+            >
+              Create Post
+            </button>
+          )}
         </div>
       )}
     </form>
