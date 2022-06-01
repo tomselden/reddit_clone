e1a73dd6890682661d60cc1fb07888ea4ce1b226
Thomas Selden <thomasselden42@gmail.com>
2022-06-01 16:54:40 -0400

    adding textbox to post and stylings

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,15 +1,67 @@
 import { useSession } from "next-auth/react";
 import React from "react";
+import Avatar from "./Avatar";
+import { LinkIcon, PhotographIcon } from "@heroicons/react/outline";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  postTitle: string;
+  postBody: string;
+  postImage: string;
+  subreddit: string;
+};
 
 function PostBox() {
   const { data: session } = useSession();
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>();
 
   return (
-    <form>
-      <div>
-          {/* AVATAR */}
-          <input type="text" placeholder="Create a host"/>
-     </div>
+    <form className="sticky top-16 z-50 border border-gray-300 bg-white p-2">
+      <div className="flex items-center space-x-3">
+        <Avatar />
+        <input
+          {...register("postTitle", { required: true })}
+          disabled={!session}
+          className="flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none"
+          type="text"
+          placeholder={
+            session ? "Create a post by entering a title." : "Sign in to post."
+          }
+        />
+
+        <PhotographIcon className="h-6 cursor-pointer text-gray-300" />
+        <LinkIcon className="h-6 text-gray-300" />
+      </div>
+
+      {!!watch("postTitle") && (
+        <div className="flex flex-col py-2">
+          {/* body */}
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Body</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register("postBody")}
+              type="text"
+              placeholder="Text (Optional)"
+            />
+          </div>
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Subreddit</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register("subreddit")}
+              type="text"
+              placeholder="i.e. NextJS"
+            />
+          </div>
+        </div>
+      )}
     </form>
   );
 }
